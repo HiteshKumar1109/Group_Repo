111fb6ef500b57c866778a016ecbacec6a00bcca
Hitesh Sharma <hs4245742@gmail.com>
2026-06-03 23:15:08 +0530

    Finalised Calculator

--- Group.py
+++ Group.py
@@ -1,5 +1,52 @@
-print("hello world")
+def power(num1, num2):
+    print(f"{num1} raised to {num2} :",num1 ** num2)
 
-print("Hi Hello")
+def modulus(ans, num):
+    print(f"{num1} mod {num2} :",num1 % num2)
 
-print("OK")
+
+print("""
+--------------Calculator Using Python--------------
+    """)
+
+num1 = int(input("Enter Number 1:"))
+num2 = int(input("Enter Number 2:"))
+
+print("""\nOperations:
+1) Addition
+2) Subtraction
+3) Multiplication
+4) Division/Floor Division
+5) Power
+6) Modulus
+      """)
+
+
+while(1):
+    op = int(input("Enter Number For Operation:"))
+    match(op):
+        case 1:
+            addition(num1,num2)   
+            break
+
+        case 2:
+            subtraction(num1,num2)
+            break
+
+        case 3:
+            multiplication(num1,num2)
+            break
+
+        case 4:
+            division(num1,num2)
+            break
+
+        case 5:
+            modulus(num1,num2)
+            break
+
+        case 6:
+            power(num1,num2)
+
+        case _:
+            print("Enter Valid Number!")
