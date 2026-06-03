10f1c34887be9db094409176ce17dc832f984f3b
HANUMANart <choudharyhanu2006@gmail.com>
2026-06-03 22:44:31 +0530

    Hanu1

--- Group.py
+++ Group.py
@@ -1,1 +1,5 @@
-print("hello world")
+def sub_number():
+    number1= int(input("The first number is"))
+    number2= int(input("The second number is"))
+    answer= number1-number2
+    print(f"The answer is :" ,answer)
