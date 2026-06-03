6b4b83dbaf6b51a6e08f7d7a8878abde5caafea9
ubiquitous-aman <amankumaraman112@gmail.com>
2026-06-03 22:50:31 +0530

    Division Code

--- Group.py
+++ Group.py
@@ -1,3 +1,12 @@
 print("hello world")
 
 print("Hi Hello")
+
+
+def division(a, b):
+    if b == 0:
+        print("Division by zero is not allowed.")
+        return
+
+    print("True Division (/):", a / b)
+    print("Floor Division (//):", a // b)
